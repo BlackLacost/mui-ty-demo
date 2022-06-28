24c17b9e4d8357bd7162e472e3ff9a53850d147f
Ilya Lisin <blacklacost@gmail.com>
2022-06-28 09:43:39 +0300

    Button

--- src/components/Dummy.jsx
+++ src/components/Dummy.jsx
@@ -1,5 +1,13 @@
+import { Button, Stack } from '@mui/material'
+
 const Dummy = () => {
-  return <div>Dummy</div>
+  return (
+    <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+  )
 }
 
 export default Dummy
