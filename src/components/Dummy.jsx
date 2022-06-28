0480e30ada043717e6f6cedd4f434728b09970e6
Ilya Lisin <blacklacost@gmail.com>
2022-06-28 10:13:04 +0300

    Container & CSS Baseline

--- src/components/Dummy.jsx
+++ src/components/Dummy.jsx
@@ -1,12 +1,14 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Container, CssBaseline } from '@mui/material'
 
 const Dummy = () => {
   return (
-    <Box sx={{ maxWidth: 500, m: 10, border: '1px solid black' }}>
-      <Typography variant="h2" component="h1" gutterBottom noWrap>
-        Heading humpty dumpty sat on a wall, humpty dumpty had a great fall
-      </Typography>
-    </Box>
+    <>
+      {/* Normalize */}
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+      </Container>
+    </>
   )
 }
 
