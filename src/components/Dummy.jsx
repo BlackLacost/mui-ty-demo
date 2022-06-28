d4a8324dc1ff88f25ecbf3f8d57de6d2046de5b7
Ilya Lisin <blacklacost@gmail.com>
2022-06-28 09:58:58 +0300

    Typography

--- src/components/Dummy.jsx
+++ src/components/Dummy.jsx
@@ -1,18 +1,12 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 
 const Dummy = () => {
   return (
-    <Box
-      sx={{
-        m: 10,
-        width: 300,
-        height: 300,
-        backgroundColor: '#000',
-        '&:hover': {
-          backgroundColor: '#bbb',
-        },
-      }}
-    />
+    <Box sx={{ maxWidth: 500, m: 10, border: '1px solid black' }}>
+      <Typography variant="h2" component="h1" gutterBottom noWrap>
+        Heading humpty dumpty sat on a wall, humpty dumpty had a great fall
+      </Typography>
+    </Box>
   )
 }
 
